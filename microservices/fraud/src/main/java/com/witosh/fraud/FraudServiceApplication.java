8771039855a251db93425007d139721a5416ab33
Witold Sobczak <witold.sobczak@comarch.com>
2023-01-08 19:12:15 +0100

    Add discovery server

--- microservices/fraud/src/main/java/com/witosh/fraud/FraudServiceApplication.java
+++ microservices/fraud/src/main/java/com/witosh/fraud/FraudServiceApplication.java
@@ -2,8 +2,10 @@ package com.witosh.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudServiceApplication.class, args);
